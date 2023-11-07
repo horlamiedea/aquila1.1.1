e3498040b5d532bb649965c976656dcdbbb356b4
Ethnos IT <114231002+aquilabyeth@users.noreply.github.com>
2023-11-07 14:11:25 +0100

    fixed patch issue

--- src/services/webSocketUrl.jsx
+++ src/services/webSocketUrl.jsx
@@ -1,6 +1,6 @@
 
 
-const socketUrl = "wss://aquilatest.livelysea-9b4d3851.westus2.azurecontainerapps.io"
+const socketUrl = "wss://aquiladev.livelysea-9b4d3851.westus2.azurecontainerapps.io"
 
 
 export default socketUrl;
